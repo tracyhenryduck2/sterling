ca83abc88af92504aa4b439982c83483b7e7f74d
ST <duckstime@sina.com>
2018-03-08 15:47:19 +0800

    的

--- Siterwell_familywell/User/Controller/Loginvc.m
+++ Siterwell_familywell/User/Controller/Loginvc.m
@@ -12,7 +12,7 @@
 
 @interface Loginvc()
 @property (nonatomic,weak) IBOutlet UIButton *test1;
-@property (nonatomic,weak) IBOutlet UIButton *test2;
+@property (nonatomic,weak) IBOutlet UIButton *savePsdbtn;
 @property (nonatomic,weak) IBOutlet UIButton *login;
 @property (nonatomic,weak) IBOutlet UITextField *username;
 @property (nonatomic,weak) IBOutlet UITextField *pwd;
@@ -27,16 +27,22 @@
     self.navigationItem.rightBarButtonItem =  [self itemWithTarget:self action:@selector(clickItem) Title:NSLocalizedString(@"ok", nil) withTintColor:RGB(53, 167, 255)];
     self.navigationItem.rightBarButtonItem.enabled = YES;
     self.navigationItem.title = @"ddd";
+    [self.navigationController setNavigationBarHidden:YES];
     _login.layer.cornerRadius = 17.5f;
     [_pwd setSecureTextEntry:YES];
+    [_savePsdbtn setImage:[UIImage imageNamed:@"unselect_remember"] forState:UIControlStateNormal];
+    [_savePsdbtn setImage:[UIImage imageNamed:@"select_remember"] forState:UIControlStateSelected];
+    [_savePsdbtn setImageEdgeInsets:UIEdgeInsetsMake(0.0, -20, 0.0, 0.0)];
+    [_savePsdbtn setSelected:YES];
 }
 
 
-////记住密码
-//- (IBAction)savePasswordAction:(id)sender {
-//    NSLog(@"test");
-//   
-//}
+//记住密码
+- (IBAction)savePasswordAction:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    [btn setSelected:![btn isSelected]];
+   
+}
 
 //登录主界面
 - (IBAction)Login:(id)sender {
